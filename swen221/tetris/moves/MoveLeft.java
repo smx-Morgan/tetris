b8f7ed4be2af7a139f642241c7456026691e2ef8
sisuy <83505734+sisuy@users.noreply.github.com>
2022-03-25 21:37:04 +0800

    fix left move bug

--- swen221/tetris/moves/MoveLeft.java
+++ swen221/tetris/moves/MoveLeft.java
@@ -1,8 +1,5 @@
 package swen221.tetris.moves;
 
-import swen221.tetris.logic.Board;
-import swen221.tetris.tetromino.ActiveTetromino;
-
 /**
  * Move the active tetromino one square to the left.
  *
@@ -15,20 +12,5 @@ public class MoveLeft extends AbstractTranslation {
 	 */
 	public MoveLeft() {
 		super(-1,0);
-	}
-
-	@Override
-	public boolean isValid(Board board) {
-		ActiveTetromino tetromino = board.getActiveTetromino();
-		if(!super.isValid(board)){
-			return false;
-		}
-
-		if(tetromino.getBoundingBox().getMinX() <= 0){
-			return false;
-		}
-
-
-		return true;
 	}
 }
--- swen221/tetris/moves/MoveRight.java
+++ swen221/tetris/moves/MoveRight.java
@@ -1,8 +1,5 @@
 package swen221.tetris.moves;
 
-import swen221.tetris.logic.Board;
-import swen221.tetris.tetromino.ActiveTetromino;
-
 /**
  * Move the active tetromino one square to the right.
  *
@@ -16,18 +13,5 @@ public class MoveRight extends AbstractTranslation {
 	 */
 	public MoveRight() {
 		super(1,0);
-	}
-
-	@Override
-	public boolean isValid(Board board) {
-		ActiveTetromino tetromino = board.getActiveTetromino();
-		if(!super.isValid(board)){
-			return false;
-		}
-		if(tetromino.getBoundingBox().getMaxX() >= board.getWidth() - 1){
-			return false;
-		}
-
-		return true;
 	}
 }
